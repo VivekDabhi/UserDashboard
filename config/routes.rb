597405a29f611b491f8532ffdbd4cf9db3ab7f6a
harsh-tntra <harsh.gajjar@tntra.io>
2022-02-17 15:38:33 +0530

    added changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :users do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
   #root "articles#index"
-  devise_for :users, controllers: { sessions: 'users/sessions'}
-  root to: "home#index"
-  devise_for :views
+  # devise_for :users, controllers: { sessions: 'users/sessions'}
+  root to: "users#index"
 end
